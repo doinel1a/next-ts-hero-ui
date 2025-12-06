78d609febaa63bbc222cfdf041809e1f146da04f
doinel1a <doinel1atanasiu@gmail.com>
2025-12-06 18:37:15 +0100

    chore: update deps & config files

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,7 @@ import './src/env.js';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
